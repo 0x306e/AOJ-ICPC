f8f1382d7c2ce49252876b9b97d02d5ad63d9810
0x306e <3so.d.na@gmail.com>
2017-07-09 01:37:53 +0900

    first commit

---
+++ src/diff100/KeitaiMessage.java
@@ -0,0 +1,28 @@
+package diff100;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class KeitaiMessage {
+
+  public static void main(String[] args) throws IOException {
+    Scanner sc = new Scanner(System.in);
+    String raw, cut;
+    String str[] = new String[76];
+
+    raw = sc.nextLine();
+    while (raw != null) {
+      
+      while () {
+        
+        
+      }
+      raw = sc.nextLine();
+      System.out.println(raw);
+    }
+
+  }
+  
+  public void
+
+}
