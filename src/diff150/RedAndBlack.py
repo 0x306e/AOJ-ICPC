1c5f918dab749db54f3c8feda5e336c761a270a5
0x306e <3so.d.na@gmail.com>
2018-07-07 03:21:28 +0900

    復習commit忘れ

--- src/diff150/RedAndBlack.py
+++ src/diff150/RedAndBlack.py
@@ -1,35 +1,28 @@
-def dfs(graph, sx, sy):
-    stack = [[sx, sy]]
-    visited = []
-    cnt = 0
-    while stack:
-        label = stack.pop(0)
-        try:
-            g = graph[label[1]][label[0]]
-            if (label[0] < 0) or (label[0] > graph[0].__len__() - 1):
-                continue
-            if (label[1] < 0) or (label[1] >graph.__len__() - 1):
-                continue
-            if g == '#':
-                continue
-            if [label[0], label[1]] not in visited:
-                # graph[label[1], label[0]] = '#'
-                cnt += 1
-                visited = [[label[0], label[1]]] + visited
-                stack = [[label[0]+1, label[1]], [label[0]-1, label[1]], [label[0], label[1]+1], [label[0], label[1]-1]] + stack
-        except IndexError:
-            pass
-    return cnt
+
+
+def dfs(x, y, mx, my):
+    global tile
+    global cnt
+    if (x < 0) or (x > mx) or (y < 0) or (y > my):
+        return
+    if tile[x][y] == '#':
+        return
+    cnt += 1
+    tile[x][y] = '#'
+    dfs(x+1, y, mx, my)
+    dfs(x-1, y, mx, my)
+    dfs(x, y+1, mx, my)
+    dfs(x, y-1, mx, my)
 
 
 if __name__ == '__main__':
-    STR = []
-    W, H = [int(i) for i in input().split()]
-    while W != 0:
-        for j in range(H):
-            STR.append(list(input()))
-            if '@' in STR[j]:
-                sx, sy = STR[j].index('@'), j
-        print(dfs(STR, sx, sy))
-        STR = []
-        W, H = [int(i) for i in input().split()]
+    global tile
+    global cnt
+    W, H = map(int, input().split())
+    tile = [input() for i in range(H)]
+    for i in range(H):
+        sx, sy = tile[i].find('@'), i
+        if sx > 0:
+            break
+    dfs(sy, sx, H, W)
+    print(cnt)
